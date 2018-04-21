e13da859dd5bb9ee08e511daa9f9e05566f841f5
Tom <toms.queiroz@gmail.com>
2018-04-21 07:46:56 -0300

    Iniciar Repositório com MainActivity

---
+++ app/src/main/java/com/example/tom/projeto3_udacity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tom.projeto3_udacity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
