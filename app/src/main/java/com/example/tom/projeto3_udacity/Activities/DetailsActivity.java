aef40270e6e218a2326a677994029942d4125db4
Tom <toms.queiroz@gmail.com>
2018-04-27 13:27:20 -0300

    Finalizar MasterListFragment

--- app/src/main/java/com/example/tom/projeto3_udacity/Activities/DetailsActivity.java
+++ app/src/main/java/com/example/tom/projeto3_udacity/Activities/DetailsActivity.java
@@ -21,50 +21,14 @@ import java.util.List;
  * Created by Tom on 21/04/2018.
  */
 
-public class DetailsActivity extends AppCompatActivity implements StepsAdapter.StepsAdapterOnClickHandler{
-
-    private Recipe recipe;
-    private TextView tv_description;
-    private RecyclerView mRecyclerView;
-    private LinearLayoutManager layoutManager;
-    private StepsAdapter mStepsAdapter;
+public class DetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        Intent i = getIntent();
-        Bundle b = i.getBundleExtra("Receita");
-        recipe = b.getParcelable("Receita");
-        tv_description = (TextView) findViewById(R.id.tv_description);
-        mRecyclerView = (RecyclerView) findViewById(R.id.rv_steps);
-        layoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(layoutManager);
-        mRecyclerView.setHasFixedSize(true);
-        mStepsAdapter = new StepsAdapter(this, this);
-        mRecyclerView.setAdapter(mStepsAdapter);
-        mStepsAdapter.setstepList(recipe.getSteps());
-        ingredientsToTextView();
-
-    }
-
-    public void ingredientsToTextView(){
-        tv_description.setText(getString(R.string.ingredientsDetailsLabel) + "\n");
-        for(Ingredient ingredient : recipe.getIngredients()){
-            tv_description.append("- " + ingredient.getIngredient());
-            tv_description.append(" (" + ingredient.getQuantity() + " " +  ingredient.getMeasure() + ")\n");
-        }
     }
 
 
-    @Override
-    public void onClick(int position) {
-        Bundle b = new Bundle();
-        b.putParcelable("Receita", recipe);
-        b.putInt("Position", position);
-        Intent intent = new Intent(this, StepDetailActivity.class);
-        intent.putExtra("Receita", b);
-        startActivity(intent);
-    }
 }
---
+++ app/src/main/java/com/example/tom/projeto3_udacity/Fragments/MasterListFragment.java
@@ -0,0 +1,78 @@
+package com.example.tom.projeto3_udacity.Fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.tom.projeto3_udacity.Activities.StepDetailActivity;
+import com.example.tom.projeto3_udacity.Adapters.StepsAdapter;
+import com.example.tom.projeto3_udacity.Model.Ingredient;
+import com.example.tom.projeto3_udacity.Model.Recipe;
+import com.example.tom.projeto3_udacity.R;
+
+/**
+ * Created by Tom on 27/04/2018.
+ */
+
+public class MasterListFragment extends Fragment implements StepsAdapter.StepsAdapterOnClickHandler{
+
+    private Recipe recipe;
+    private TextView tv_description;
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager layoutManager;
+    private StepsAdapter mStepsAdapter;
+    private View rootView;
+
+
+    public MasterListFragment(){
+
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
+        Intent i = getActivity().getIntent();
+        Bundle b = i.getBundleExtra("Receita");
+        recipe = b.getParcelable("Receita");
+        tv_description = (TextView) rootView.findViewById(R.id.tv_description);
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.rv_steps);
+        layoutManager = new LinearLayoutManager(rootView.getContext());
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+        mStepsAdapter = new StepsAdapter(this, rootView.getContext());
+        mRecyclerView.setAdapter(mStepsAdapter);
+        mStepsAdapter.setstepList(recipe.getSteps());
+        ingredientsToTextView();
+        return rootView;
+    }
+
+    public void ingredientsToTextView(){
+        tv_description.setText(getString(R.string.ingredientsDetailsLabel) + "\n");
+        for(Ingredient ingredient : recipe.getIngredients()){
+            tv_description.append("- " + ingredient.getIngredient());
+            tv_description.append(" (" + ingredient.getQuantity() + " " +  ingredient.getMeasure() + ")\n");
+        }
+    }
+
+
+    @Override
+    public void onClick(int position) {
+        Bundle b = new Bundle();
+        b.putParcelable("Receita", recipe);
+        b.putInt("Position", position);
+        Intent intent = new Intent(rootView.getContext(), StepDetailActivity.class);
+        intent.putExtra("Receita", b);
+        startActivity(intent);
+    }
+
+}
